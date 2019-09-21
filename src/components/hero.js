b59b6e5dbd742f7d36a97d37f2d8358f08a1790a
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-21 19:56:26 -0400

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -26,10 +26,9 @@ const Header = ({ siteTitle }) => (
         opacity: 0.5,
       }}
     />
-    <h1 style={{ textAlign: "center" }}>Landing Page Starter</h1>
+    <h1 style={{ textAlign: "center" }}>Farmore</h1>
     <p style={{ textAlign: "center", maxWidth: 440 }}>
-      This landing page looks great on all devices and is minimal in design. Add
-      what you want and deploy.
+      Connect with your local farmer's market
     </p>
     <Button>Get Early Access</Button>
     <div style={{ margin: 60, width: `250px`, position: "relative" }}>
