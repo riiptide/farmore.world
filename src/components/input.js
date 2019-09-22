6513483daa45030f0a04247ca23637ceb936ea61
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-22 10:47:56 -0400

    Create input.js

---
+++ src/components/input.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import { COLORS, BORDER_RADIUS, GRADIENT } from "../styles/constants"
+import "../styles/button.css"
+
+const Button = ({ children }) => (
+  <inoput
+    style={{
+      padding: ".5rem 2.5rem",
+      
+      borderRadius: BORDER_RADIUS,
+      borderWidth: 0,
+      marginRight:3px;
+      marginLeft:3px;
+      marginBottom:3px;
+      marginTop:3px;
+
+
+
+    }}
+  >
+    {children}
+  </input>
+)
+
+export default Button
