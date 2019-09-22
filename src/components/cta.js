367fdb53be44901b0fa1f5b44ee86170d6330b34
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-22 09:45:39 -0400

    cta.js update input style

--- src/components/cta.js
+++ src/components/cta.js
@@ -9,23 +9,27 @@ const CallToAction = () => (
       title="Stay Updated"
       description="Signup to know when the farmore app is ready for download."
     />
-  style = {{
-  input {
-margin-right:3px;
-margin-right:3px;
-margin-bottom:3px;
-margin-top:3px;
-border-radius: 10px;
-}
-  }}
+
   <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
 
                     <input type="hidden" name="bot-field" />
 
                     <div>
-                        <input type="text" name="name" id="name" placeholder=" Name "required/>
+                        <input style={{
+margin-right:3px;
+margin-right:3px;
+margin-bottom:3px;
+margin-top:3px;
+border-radius: 10px;
+}} type="text" name="name" id="name" placeholder=" Name "required/>
                        
-                        <input type="text" name="email" id="email" placeholder=" email "required/>
+                        <input style={{
+margin-right:3px;
+margin-right:3px;
+margin-bottom:3px;
+margin-top:3px;
+border-radius: 10px;
+}} type="text" name="email" id="email" placeholder=" email "required/>
                     </div>
                         <Button onclick="this.form.submit()">Get Early Access</Button>
 
