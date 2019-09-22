90dd0a7468ed861635d893781e8a3b7d5b680df7
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-21 22:48:28 -0400

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from "../components/button"
-import SectionHeader from "./section-header"
+import SectionHeader from "../components/section-header"
 
 const Contact = (props) => (
     
