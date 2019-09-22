26eb913ce55a0ec4c2fd809957676c25cdeac482
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-21 22:53:42 -0400

    Update cta.js

--- src/components/cta.js
+++ src/components/cta.js
@@ -9,7 +9,24 @@ const CallToAction = () => (
       title="Stay Updated"
       description="Give a final call to action because that's what the cool kids are doing."
     />
-    <Button>Get Early Access</Button>
+  <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+
+                    <input type="hidden" name="bot-field" />
+
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" required/>
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" required/>
+                    </div>
+                        <input type="submit" value="Send Message" className="special" />
+                        <Button onclick="this.form.submit()">Get Early Access</Button>
+
+
+        </form>
+ <!--     <Button>Get Early Access</Button>-->
   </div>
 )
 
