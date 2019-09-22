9af283e2bd82cfc6495ef23415dea391e0f095a9
riiptide <29617525+riiptide@users.noreply.github.com>
2019-09-21 21:50:00 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ const Footer = ({ siteTitle }) => (
       <div style={{ color: COLORS.blue, fontWeight: 700 }}>
         <a
           style={{ textDecoration: "none" }}
-          href="https://github.com/gillkyle/gatsby-starter-landing-page"
+          href="http://farmore.world"
         >
           Contact Us
         </a>
